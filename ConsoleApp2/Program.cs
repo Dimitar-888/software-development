6575c8c5c0304bcd05565bc41f25f592de3b1716
Dimitar-888 <d65676737@gmail.com>
2026-09-24 13:30:37 +0300

    new project added

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class TaskItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public TaskItem(string title, string description, DateTime deadline)
+        {
+            Title = title;
+            Description = description;
+            Deadline = deadline;
+            IsCompleted = false;
+        }
+
+    }
+}
